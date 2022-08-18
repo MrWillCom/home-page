c87391cf6937363d1bc9a6d8796383606ffb656e
Mr. Will <mr.will.com@outlook.com>
2022-08-18 22:17:56 +0800

    Remove old code

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-const updateScrollProgress = () => {
-    document.body.style.setProperty('--body-scroll-top-px', document.body.scrollTop + 'px')
-    document.body.style.setProperty('--body-scroll-top-1', (document.body.scrollTop / (document.body.scrollHeight - document.body.clientHeight)))
-    document.body.style.setProperty('--body-scroll-top-percent', (document.body.scrollTop / (document.body.scrollHeight - document.body.clientHeight) * 100) + '%')
-}
-
-updateScrollProgress()
-window.addEventListener('scroll', updateScrollProgress)
