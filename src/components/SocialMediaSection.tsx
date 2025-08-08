aa475d7ec602fecae2e65784ad484a3c925f77f8
Mr. Will <mr.will.com@outlook.com>
2025-08-08 10:34:19 +0800

    Change order of social media

--- src/components/SocialMediaSection.tsx
+++ src/components/SocialMediaSection.tsx
@@ -6,11 +6,11 @@ export default function SocialMediaSection() {
       <a href="https://github.com/MrWillCom" target="_blank">
         <span>GitHub</span>
       </a>
-      <a href="https://noc.social/@MrWillCom" target="_blank">
-        <span>Mastodon</span>
-      </a>
       <a href="mailto:hi@mrwillcom.com">
         <span>Email</span>
+      </a>
+      <a href="https://noc.social/@MrWillCom" target="_blank">
+        <span>Mastodon</span>
       </a>
       <a href="https://unsplash.com/@mrwillcom" target="_blank">
         <span>Unsplash</span>
