1b1c1e59afa8df322493b6c4cd083aebc3bf69a5
Mr. Will <mr.will.com@outlook.com>
2025-10-09 23:09:49 +0800

    Add a banner for Reneur’s waitlist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import ProjectShowcase from '@/components/ProjectShowcase'
 import FeatureSupportSection from '@/components/FeatureSupportSection'
 import TimeSection from '@/components/TimeSection'
 import AnimeShowcase from '@/components/AnimeShowcase'
+import TextBanner from '@/components/TextBanner'
 
 export default function Home() {
   return (
@@ -37,6 +38,7 @@ export default function Home() {
         </div>
       </section>
       <SocialMediaSection />
+      <TextBanner />
       <TimeSection />
       <NavigatorSection />
       <TechShowcase />
---
+++ src/components/TextBanner.tsx
@@ -0,0 +1,28 @@
+import styles from './TextBanner.module.scss'
+
+// this is a temporary banner for Reneur's waitlist
+
+export default function TextBanner() {
+  return (
+    <section className={styles.sect}>
+      <p>
+        <strong>Reneur</strong> is a new links page.
+      </p>
+      <p>
+        Unlike Linktree or Bio Link, it's <strong>compact</strong> and{' '}
+        <strong>efficient</strong>, which is so suitable{' '}
+        <i>
+          <strong>for developers</strong>
+        </i>
+        .
+      </p>
+      <p>
+        To be the first to know when it launches,{' '}
+        <a href="https://wt.ls/reneur" target="_blank">
+          join the waitlist
+        </a>
+        .
+      </p>
+    </section>
+  )
+}
